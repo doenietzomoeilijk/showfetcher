8ec7c08f2a67ef8e4357c803871ea83acbf0896a
Max Roeleveld <doenietzomoeilijk@gmail.com>
2017-03-06 08:33:27 +0100

    A little documentation, a little streamlining

--- main.go
+++ main.go
@@ -1,52 +1,45 @@
+// Showfetcher is a simple glue layer between ShowRSS.info and your Transmission bittorrent client. See README.md in the Github repo for more information.
 package main
 
 import (
 	"log"
 
 	"github.com/doenietzomoeilijk/showfetcher/config"
-	"github.com/doenietzomoeilijk/showfetcher/episode"
 	"github.com/doenietzomoeilijk/showfetcher/feed"
 	"github.com/doenietzomoeilijk/showfetcher/storage"
 	"github.com/doenietzomoeilijk/showfetcher/torrent"
 )
 
-var shows []episode.Show
-
 func main() {
+	log.Println("Getting config, setting up storage and Transmission client...")
 	conf := config.Load()
-
 	storage.Setup()
 	defer storage.Close()
-
 	torrent.Setup(
 		conf.Transmission,
 		conf.IncompleteDir,
 	)
 
-	// Find and clean the torrents we already had going
+	log.Println("Find and clean the torrents we already had going...")
 	episodes, err := torrent.Cleanup()
 	if err != nil {
 		log.Fatalln("Couldn't run torrent cleanup:", err)
 		return
 	}
-
 	doneEps := len(episodes)
 	storage.MarkDone(episodes)
 
-	// Fill the db with show info, based on the RSS feed.
+	log.Println("Fill the database with show info, based on the RSS feed...")
 	episodes = feed.Parse(conf.FeedURL)
 	feedEps := len(episodes)
 	storage.Store(episodes)
 
-	// Now fetch anything with status new and add it to the tracker
+	log.Println("Figuring out new episodes and adding them to the tracker...")
 	episodes = storage.Get()
 	newEps := len(episodes)
 	torrent.Add(episodes)
 
 	ts := torrent.List()
-	for _, t := range ts {
-		log.Println(t.Status, t.Name, t.PercentDone, t.IsFinished, t.RateUpload)
-	}
 	torEps := len(ts)
 
 	if err != nil {
